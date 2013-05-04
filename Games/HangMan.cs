bad01b11d825ac2591635fdef490652cfc601aa5
Mark Provenzano <kj.provenzano@gmail.com>
2013-05-03 22:39:18 -0400

    New stuff
    Dont know what though

--- Games/HangMan.cs
+++ Games/HangMan.cs
@@ -8,5 +8,27 @@ namespace Games
 {
     class HangMan
     {
+        public void Start()
+        {
+
+        }
+
+        private void setWordBank()
+        {
+
+        }
+
+        private static void player()
+        {
+        
+        }
+
+        private void letterCheck()
+        {
+
+        }
+
+        private List<String> wordBank = new List<string>();
+
     }
 }
